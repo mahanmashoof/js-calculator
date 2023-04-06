b627ca215b4beaef24deda4b26aa5c243f0ca0cb
Mahan Mashoof <mahan.mashoof@gmail.com>
2023-04-06 11:36:47 -0300

    add paranthesis and delete button

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 let expression = "";
 
-// Function to add value to expression
-function addToExpression(button) {
+const addToExpression = (button) => {
   expression += button.getAttribute("data-value");
   document.getElementById("result").value = expression;
-}
+};
 
-// Function to clear expression
-function clearExpression() {
+const delLast = () => {
+  expression = expression.substring(0, expression.length - 1);
+  document.getElementById("result").value = expression;
+};
+
+const clearExpression = () => {
   expression = "";
   document.getElementById("result").value = expression;
-}
+};
 
-// Function to calculate expression
-function calculateExpression() {
+const calculateExpression = () => {
   try {
     expression = expression.length < 1 ? "" : eval(expression);
     document.getElementById("result").value = expression;
   } catch (e) {
     alert("Invalid expression");
   }
-}
+};
